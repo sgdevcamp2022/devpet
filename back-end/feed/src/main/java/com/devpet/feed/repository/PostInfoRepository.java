be1b5641faa723957c129711a52a6a9c1f3892d3
오세환 <osehwan@osehwan-ui-MacBookAir.local>
2023-02-13 18:41:06 +0900

    FEAT: petcontroller 추가.

--- back-end/feed/src/main/java/com/devpet/feed/repository/PostInfoRepository.java
+++ back-end/feed/src/main/java/com/devpet/feed/repository/PostInfoRepository.java
@@ -3,7 +3,6 @@ package com.devpet.feed.repository;
 import com.devpet.feed.model.entity.PostInfo;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
