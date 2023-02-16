a91cd342b8e6771dfe60bd56cbfc39fca6a71d29
pentel1 <chs5310@naver.com>
2023-02-17 06:58:50 +0900

    FEAT:홈 화면 마커 추가 및 프로필 데이터 불러오기 기능 구현

---
+++ front-end/app/src/main/java/com/example/petmily/model/data/make/remote/Tag.java
@@ -0,0 +1,17 @@
+package com.example.petmily.model.data.make.remote;
+
+public class Tag {
+    String hashTag;
+
+    public Tag(String hashTag) {
+        this.hashTag = hashTag;
+    }
+
+    public String getHashTag() {
+        return hashTag;
+    }
+
+    public void setHashTag(String hashTag) {
+        this.hashTag = hashTag;
+    }
+}
---
+++ front-end/app/src/main/java/com/example/petmily/view/Activity_PostFull.java
@@ -0,0 +1,11 @@
+package com.example.petmily.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity_PostFull  extends AppCompatActivity {
+
+
+
+
+
+}
