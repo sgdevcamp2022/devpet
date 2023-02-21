555fc74e9899e8549ec486a0386f92fd0465c222
오세환 <osehwan@osehwan-ui-MacBookAir.local>
2023-02-21 17:10:53 +0900

    Fix: enable feed eureka & create api-gateway application.yml

--- back-end/feed/src/main/java/com/devpet/feed/FeedApplication.java
+++ back-end/feed/src/main/java/com/devpet/feed/FeedApplication.java
@@ -3,11 +3,11 @@ package com.devpet.feed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class FeedApplication {
 
     public static void main(String[] args) {
