deee7aecc8c6a1f75ab3bfbc7faa7e9c615bff65
acs0209 <annjae4676@naver.com>
2023-02-22 02:36:52 +0900

    CHORE : UserInfoRepository 쿼리문 수정
    1. 팔로우한 유저가 작성한 게시물 쿼리 추가
    
    2. getPetLikeCommentPostList, getRecommendedFollowPostList
    메소드 쿼리문 수정

--- back-end/feed/src/main/java/com/devpet/feed/repository/UserInfoRepository.java
+++ back-end/feed/src/main/java/com/devpet/feed/repository/UserInfoRepository.java
@@ -133,19 +133,25 @@ public interface UserInfoRepository extends Neo4jRepository<UserInfo, String> {
             "where duration.inSeconds(date, now).hours < 10" +
             "return p.postId")
     List<String> getFollowingNewPostList(String userId);
-    // 유저가 좋아요, 댓글, 키우는 펫과 관련된 태그의 게시물(주황색 부분)
-    @Query("match (u:UserInfo{userId: $userId})-[:PET]->()-[:TAGD]->(:Tag)<-[:TAGD]-(p:PostInfo) " +
-            "WITH p, datetime() AS now, p.createdAt AS date " +
+
+    // 유저가 좋아요, 댓글, 키우는 펫과 관련된 태그의 게시물 + 팔로우한 유저가 작성한 게시물(주황색 부분)
+    @Query("match (u1:UserInfo{userId: $userId})-[:FOLLOW]->()-[:POST]->(p1:PostInfo) " +
+            "WITH p1, datetime() AS now, datetime(p1.createdAt) AS date " +
+            "where duration.inSeconds(date, now).hours < 24 " +
+            "return p1.postId as postId " +
+            "union " +
+            "match (u:UserInfo{userId: $userId})-[:PET]->()-[:TAGD]->(:Tag)<-[:TAGD]-(p:PostInfo) " +
+            "WITH p, datetime() AS now, datetime(p.createdAt) AS date " +
             "where duration.inSeconds(date, now).hours < 24 " +
             "return p.postId as postId " +
             "union " +
             "match (u1:UserInfo{userId: $userId})-[:LIKE]->(p1:PostInfo)-[:TAGD]->(t1:Tag)<-[:TAGD]-(n1:PostInfo) " +
-            "WITH n1, datetime() AS now, n1.createdAt AS date " +
+            "WITH n1, datetime() AS now, datetime(n1.createdAt) AS date " +
             "where duration.inSeconds(date, now).hours  < 24 " +
             "return n1.postId as postId " +
             "union " +
             "match (u2:UserInfo{userId: $userId})-[:COMMENT]->(p2:PostInfo)-[:TAGD]->(t2:Tag)<-[:TAGD]-(n2:PostInfo) " +
-            "WITH n2, datetime() AS now, n2.createdAt AS date " +
+            "WITH n2, datetime() AS now, datetime(n2.createdAt) AS date " +
             "where duration.inSeconds(date, now).hours  < 24 " +
             "return n2.postId as postId ")
     Set<String> getPetLikeCommentPostList(@Param("userId") String userId);
@@ -154,27 +160,27 @@ public interface UserInfoRepository extends Neo4jRepository<UserInfo, String> {
     @Query("Match(u1:UserInfo{userId: $userId})-[:FOLLOW]->()-[r1:RECOMMENDED]->(p1:PostInfo) " +
             "with r1, p1 " +
             "ORDER BY r1.score DESC " +
-            "LIMIT 4 " +
+            "LIMIT 6 " +
             "MATCH (p1)-[:TAGD]->(:Tag)<-[:TAGD]-(n1:PostInfo) " +
-            "WITH n1, datetime() AS now, n1.createdAt AS date " +
+            "WITH n1, datetime() AS now, datetime(n1.createdAt) AS date " +
             "where duration.inSeconds(date, now).hours < 24 " +
             "return n1.postId as postId " +
             "union " +
             "match(u2:UserInfo{userId : $userId})-[r2:RECOMMENDED]->(p2:PostInfo) " +
             "with r2, p2 " +
             "ORDER BY r2.score DESC " +
-            "LIMIT 4 " +
+            "LIMIT 6 " +
             "match (p2)-[:TAGD]->(:Tag)<-[:TAGD]-(n2:PostInfo) " +
-            "WITH n2, datetime() AS now, n2.createdAt AS date " +
+            "WITH n2, datetime() AS now, datetime(n2.createdAt) AS date " +
             "where duration.inSeconds(date, now).hours < 24 " +
             "return n2.postId as postId " +
             "union " +
             "Match (u:UserInfo{userId: $userId})-[:FOLLOW]->()-[f:FOLLOW]-()-[:POST]->(p:PostInfo) " +
             "Match (n:PostInfo)<-[:RECOMMENDED]-(u) " +
             "with n " +
-            "limit 4 " +
+            "limit 6 " +
             "MATCH (p)-[:TAGD]->(:Tag)<-[:TAGD]-(n:PostInfo) " +
-            "with p, datetime() AS now, p.createdAt AS date " +
+            "with p, datetime() AS now, datetime(p.createdAt) AS date " +
             "where duration.inSeconds(date, now).hours < 24 " +
             "return DISTINCT p.postId as postId ")
     Set<String> getRecommendedFollowPostList(@Param("userId") String userId);
